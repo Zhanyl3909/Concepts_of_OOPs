b663806a293e4c6d325deddd3de16618e53196b8
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-05-22 17:08:51 +0900

    Update Main.java

---
+++ Inheritance/Main.java
@@ -0,0 +1,84 @@
+
+//Repeating 
+class Rectangle {
+  private int x;
+  private int y;
+  private int weight;
+  private int height;
+
+public Rectangle() {
+  this.x = 0;
+  this.y = 0;
+  this.weight = 0;
+  this.height = 0;
+}
+
+public Rectangle(int weight, int height) {
+  this.x = 0;
+  this.y = 0;
+  this.weight = weight;
+  this.height = height;
+}
+
+public Rectangle(int x, int y, int weight, int height) {
+  this.x = x;
+  this.y = y;
+  this.weight = weight;
+  this.height = height;
+}
+
+}
+
+//Chaining Constructor
+
+class Rectangle {
+  private int x;
+  private int y;
+  private int weight;
+  private int height;
+
+public Rectangle() {
+  this(0,0)
+}
+
+public Rectangle(int weight, int height) {
+  this(0,0, weight, height)
+}
+
+//the 3rd constructor does all the work
+public Rectangle(int x, int y, int weight, int height) {
+  this.x = x;
+  this.y = y;
+  this.weight = weight;
+  this.height = height;
+}
+
+}
+
+
+//super 
+class Shape {
+  private int x;
+  private int y;
+  
+
+public Shape(int x, int y) {
+	this.x = x;
+	this.y = y;
+  }
+}
+
+class Rectangle extends Shape {
+	 private int width;
+	 private int height;
+
+public Rectangle(int x, int y) {
+	this(x, y, 0, 0);
+}
+
+public Rectangle(int x, int y, int width, int height) {
+	 super(x,y);
+	 this.width = width;
+	 this.height = height;
+  }
+}
