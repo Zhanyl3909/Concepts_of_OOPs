b309304cdac8fd5e20533e9c6a9c47252e62c89e
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-06-11 21:00:25 +0900

    Update Main.java

---
+++ Stack_by_Hand/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyStack a = new MyStack();
+        MyStack b = new MyStack();
+
+        a.push(10);
+        a.push(20);
+        int x = a.pop();
+        a.push(30);
+        a.push(40);
+
+        b.push(100);
+        b.push(200);
+        b.push(300);
+        b.push(400);
+        int y = b.pop();
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        System.out.println("top of a stack a = " + a.peek());
+        System.out.println("top of a stack b = " + b.peek());
+
+        System.out.println("Size of Stack a before reset = " + a.size());
+        a.clear();
+        System.out.println("Size of Stack a after reset = " + a.size());
+
+    }
+}
