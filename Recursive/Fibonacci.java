24ccd3367819e5832938977ae39200b37be97fc6
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-06-11 18:52:42 +0900

    Update Fibonacci.java

---
+++ Recursive/Fibonacci.java
@@ -0,0 +1,23 @@
+
+public class Fibo {
+
+    static int fibo(int n)  {
+        try {
+
+            if(n == 0) return 1;
+            if(n == 1) return 1;
+            if(n > 2) return fibo(n-1) + fibo(n-2);
+
+
+        } catch (Exception e) {
+
+          System.out.println("error");
+        }
+        return 0;
+    }
+    public static void main(String[] args)  {
+        for(int i=0; i<=10; i++) {
+            System.out.println("F(" + i +") = " + fibo(i));
+        }
+    }
+}
