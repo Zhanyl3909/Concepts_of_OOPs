b2b68b8d05391873682fd74ad019219370030b17
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-07-17 18:20:18 +0900

    Create For_Each_Loop.java

---
+++ Array/For_Each_Loop.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       int[] newArray;
+       newArray = new int[5];
+
+       for(int i=0; i< newArray.length; i++) {
+           newArray[i] = newArray.length - i;
+           System.out.print(newArray[i] + " ");
+       }
+
+       System.out.println();
+
+       for(int element : newArray) {
+           System.out.print(element +  " ");
+       }
+
+    }
+}
