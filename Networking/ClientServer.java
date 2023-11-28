2c5291bf84e95bf974d58ea1bdf7d2651be01b84
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-11-28 15:11:14 +0900

    Update ClientServer.java

---
+++ Networking/ClientServer.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(3000)) {   // any integer (0~65535) 
+            Socket socket = serverSocket.accept();  //going to block until a client connects to the server
+            System.out.println("Client connected");
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true) {
+                String echoString = input.readLine();
+                if(echoString.equals("exit")) {
+                    break;
+                }
+                output.println("Echo form server: " + echoString);
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+          //  e.printStackTrace();
+        }
+    }
+}
