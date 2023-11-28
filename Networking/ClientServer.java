3f4002a643b6ebf02ee0c1d9436624ffdc46fab4
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-11-28 15:11:44 +0900

    Update ClientServer.java

--- Networking/ClientServer.java
+++ Networking/ClientServer.java
@@ -2,28 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
+//Client socket
 public class Main {
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(3000)) {   // any integer (0~65535) 
-            Socket socket = serverSocket.accept();  //going to block until a client connects to the server
-            System.out.println("Client connected");
-            BufferedReader input = new BufferedReader(
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            BufferedReader echoes = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(),true);
 
-            while(true) {
-                String echoString = input.readLine();
-                if(echoString.equals("exit")) {
-                    break;
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String responses;
+
+            do {
+                System.out.println("Enter String to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+                if(!echoString.equals("exit")) {
+                    responses = echoes.readLine();
+                    System.out.println(responses);
                 }
-                output.println("Echo form server: " + echoString);
-            }
-        } catch (IOException e) {
-            System.out.println("Server exception " + e.getMessage());
-          //  e.printStackTrace();
+            } while (!echoString.equals("exit"));
+
+        }catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
         }
     }
 }
